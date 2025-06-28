da6b2007145bca954373c2bd7d65677fc1e272cc
Atanas Yonkov <yonkov.atanas@gmail.com>
2025-06-28 13:05:08 +0300

    Update features pattern

--- patterns/06-features.php
+++ patterns/06-features.php
@@ -4,9 +4,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-$content = '<!-- wp:group {"align":"full","layout":{"type":"constrained","contentSize":"980px","wideSize":"1180px"}} -->
-<div class="wp-block-group alignfull"><!-- wp:columns {"className":"nasio-pattern","style":{"spacing":{"blockGap":{"top":"2rem","left":"1.25rem"},"padding":{"top":"2rem","bottom":"2rem"}}}} -->
-<div class="wp-block-columns nasio-pattern" style="padding-top:2rem;padding-bottom:2rem"><!-- wp:column -->
+$content = '<!-- wp:group {"align":"full","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull"><!-- wp:columns {"className":"nasio-pattern","style":{"spacing":{"blockGap":{"top":"2rem","left":"1.25rem"},"padding":{"top":"2rem","bottom":"2rem","left":"1rem","right":"1rem"}}}} -->
+<div class="wp-block-columns nasio-pattern" style="padding-top:2rem;padding-right:1rem;padding-bottom:2rem;padding-left:1rem"><!-- wp:column -->
 <div class="wp-block-column"><!-- wp:nasio-block/icon-block {"icon":"brush","iconSize":32,"itemsJustification":"left","style":{"elements":{"link":{"color":{"text":"#3a72d3"}}},"color":{"text":"#3a72d3"}}} -->
 <div class="wp-block-nasio-block-icon-block wp-block-nasio-block-icon has-text-color has-link-color" style="color:#3a72d3;--icon-size:32px;--background-color:transparent;--text-color:currentColor;--border-radius:0;--padding:0"><div class="nasio-icon-wrapper is-justified-left"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true"><path d="M4 20h8v-1.5H4V20zM18.9 3.5c-.6-.6-1.5-.6-2.1 0l-7.2 7.2c-.4-.1-.7 0-1.1.1-.5.2-1.5.7-1.9 2.2-.4 1.7-.8 2.2-1.1 2.7-.1.1-.2.3-.3.4l-.6 1.1H6c2 0 3.4-.4 4.7-1.4.8-.6 1.2-1.4 1.3-2.3 0-.3 0-.5-.1-.7L19 5.7c.5-.6.5-1.6-.1-2.2zM9.7 14.7c-.7.5-1.5.8-2.4 1 .2-.5.5-1.2.8-2.3.2-.6.4-1 .8-1.1.5-.1 1 .1 1.3.3.2.2.3.5.2.8 0 .3-.1.9-.7 1.3z"></path></svg></div></div>
 <!-- /wp:nasio-block/icon-block -->
